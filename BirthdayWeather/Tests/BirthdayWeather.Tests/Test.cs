46a7e9a7722dc9621712e83a5dca71ed3cf3a4bd
Ed Snider <ed.snider@infernored.com>
2019-01-01 13:40:23 -0500

    added blank unit test project

---
+++ BirthdayWeather/Tests/BirthdayWeather.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace BirthdayWeather.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
